48a0d2bbb37fd174ea0f234783c69589cf269edd
Cristiano Junior <cristianojunior120@gmail.com>
2023-03-19 19:16:15 -0300

    implementado métodos try catch

--- pilha.php
+++ pilha.php
@@ -3,13 +3,24 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+    // $divisao = intdiv(5,0);
+    try {
+        funcao2();
+    }
+    catch (RuntimeException){
+        echo "Na função 1 eu resolvi o problema da função 2." . PHP_EOL;
+    }
+    
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Algo aqui';
+
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
     }
